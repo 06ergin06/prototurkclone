8db9447a02e0b5d879f69c867a792b5dd8f2120e
Webtas <72016401+06ergin06@users.noreply.github.com>
2021-12-18 20:27:22 +0300

    prototurk

---
+++ public/tailwind.config.js
@@ -0,0 +1,42 @@
+const _ = require('lodash')
+const plugin = require('tailwindcss/plugin')
+module.exports = {
+  mode: 'jit',
+  purge: ['./**/*.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+       prototurk : '#32445a',
+       twitter : '#1da1f2'      
+      },
+      spacing: {
+        '15': '3.75rem' 
+      },
+      zIndex: {
+        '-1': '-1'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function({ addUtilities, theme, e }) {
+      const calcUtilities = _.map(theme('spacing'), (value, key) => {
+        return {
+          [`.${e(`calc-h-full-${key}`)}`]: {
+            height: `calc(100% - ${value})`
+          },
+          [`.${e(`calc-w-full-${key}`)}`]: {
+            width: `calc(100% - ${value})`
+          }
+        }
+      })
+
+      addUtilities(calcUtilities, {
+        variants: ['responsive', 'hover'],
+      })
+    })
+  ],
+}
